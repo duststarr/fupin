cd3687a53672b9052ee6aba79f585b6643c9472b
duststarr <duststarr@hotmail.com>
2020-04-10 22:43:05 +0800

    build 自动上传到服务器

--- build/index.js
+++ build/index.js
@@ -3,35 +3,24 @@ const chalk = require('chalk')
 const config = require('../vue.config.js')
 const rawArgv = process.argv.slice(2)
 const args = rawArgv.join(' ')
+const client = require('scp2')
 
+run(`vue-cli-service build ${args}`)
 
-if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
-  const report = rawArgv.includes('--report')
-
-  run(`vue-cli-service build ${args}`)
-
-  const port = 9526
-  const publicPath = config.publicPath
-
-  var connect = require('connect')
-  var serveStatic = require('serve-static')
-  const app = connect()
-
-  app.use(
-    publicPath,
-    serveStatic('./dist', {
-      index: ['index.html', '/']
-    })
-  )
-
-
-  app.listen(port, function () {
-    console.log(chalk.green(`> Preview at  http://localhost:${port}${publicPath}`))
-    if (report) {
-      console.log(chalk.green(`> Report at  http://localhost:${port}${publicPath}report.html`))
-    }
-
-  })
-} else {
-  run(`vue-cli-service build ${args}`)
-}
+console.log(chalk.cyan('  Build complete.\n'))
+console.log(chalk.yellow(
+  '  Tip: built files are meant to be served over an HTTP server.\n' +
+  '  Opening index.html over file:// won\'t work.\n'
+))
+client.scp('./dist', {
+  host: '49.233.70.235',
+  username: 'root',
+  password: 'xxzxB105_2012',
+  path: '/www/wwwroot/fupin.howeaver.cn'
+}, function (err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('文件上传完毕!\n')
+  }
+})
